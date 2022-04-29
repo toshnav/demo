aed89935e1cdbf4903ee1c098e904279ba6d3fe7
Toshnav Vinayak Khatke <56795862+toshnav@users.noreply.github.com>
2022-04-29 14:17:14 +0530

    aa

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
 function NameList() {
-    const names =['Bruec','Toshnav','Damin']
-  return (
-      <div>
-          <h2>{names[0]}</h2>
-          <h2>{names[1]}</h2>
-          <h2>{ names[2]}</h2>
-    </div>
-  )
+    const names = ['Bruec', 'Toshnav', 'Damin']
+    const nameList = names.map(name => <h2>{name}</h2>)
+    return <div>{nameList}</div>
 }
 
 export default NameList
